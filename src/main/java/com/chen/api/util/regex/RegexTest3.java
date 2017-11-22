6cb78912ac0af2e1328c9eab0a0d0cfe1bef2bae
chenweijie <chenweijie@sunlands.com>
2017-11-22 16:57:43 +0800

    update RegexTest3

--- src/main/java/com/chen/api/util/regex/RegexTest3.java
+++ src/main/java/com/chen/api/util/regex/RegexTest3.java
@@ -54,13 +54,13 @@ public class RegexTest3 {
     public void test3() {
 
         String poem =
-                "Twas brillig, and the slithy toves/n" +
-                        "Did gyre and gimble in the wabe./n" +
-                        "All mimsy were the borogoves,/n" +
-                        "And the mome raths outgrabe./n/n" +
-                        "Beware the Jabberwock, my son,/n" +
-                        "The jaws that bite, the claws that catch./n" +
-                        "Beware the Jubjub bird, and shun/n" +
+                "Twas brillig, and the slithy toves\n" +
+                        "Did gyre and gimble in the wabe.\n" +
+                        "All mimsy were the borogoves,\n" +
+                        "And the mome raths outgrabe.\n\n" +
+                        "Beware the Jabberwock, my son,\n" +
+                        "The jaws that bite, the claws that catch.\n" +
+                        "Beware the Jubjub bird, and shun\n" +
                         "The frumious Bandersnatch.";
 
         Pattern pattern = Pattern.compile("(?m)(\\S+)\\s+((\\S+)\\s+(\\S+))");
@@ -68,8 +68,43 @@ public class RegexTest3 {
         Matcher matcher = pattern.matcher(poem);
 
         while (matcher.find()) {
-            for (int i = 0; i <= matcher.groupCount(); i++) {
-                System.out.println(matcher.group(i));
+            int count = matcher.groupCount();
+            for (int i = 0; i <= count; i++) {
+                String result = matcher.group(i);
+                System.out.println(result);
+            }
+        }
+    }
+
+
+    @Test
+    public void test4() {
+        String[] input = new String[]{"Java has regular expressions in 1.4", "regular expressions now expressing in Java", "Java represses oracular expressions"};
+        Pattern p1 = Pattern.compile("re\\w*"),
+                p2 = Pattern.compile("Java.*");
+        for (int i = 0; i < input.length; i++) {
+            System.out.println("input " + i + ": " + input[i]);
+            Matcher m1 = p1.matcher(input[i]),
+                    m2 = p2.matcher(input[i]);
+            while (m1.find()) {
+                System.out.println("m1.find() '" + m1.group() + "' start = " + m1.start() + " end = " + m1.end());
+            }
+            while (m2.find()) {
+                System.out.println("m2.find() '" + m2.group() + "' start = " + m2.start() + " end = " + m2.end());
+            }
+            // No reset() necessary
+            if (m1.lookingAt()) {
+                System.out.println("m1.lookingAt() start = " + m1.start() + " end = " + m1.end());
+            }
+            if (m2.lookingAt()) {
+                System.out.println("m2.lookingAt() start = " + m2.start() + " end = " + m2.end());
+            }
+            // No reset() necessary
+            if (m1.matches()) {
+                System.out.println("m1.matches() start = " + m1.start() + " end = " + m1.end());
+            }
+            if (m2.matches()) {
+                System.out.println("m2.matches() start = " + m2.start() + " end = " + m2.end());
             }
         }
     }
