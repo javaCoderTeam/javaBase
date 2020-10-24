47247631d12dc0f08477d31073d050116d24160a
chenweijie <chenweijie@sunlands.com>
2020-10-24 23:22:17 +0800

    其它解法

---
+++ src/main/java/com/chen/algorithm/study/test25/ListNode.java
@@ -0,0 +1,26 @@
+package com.chen.algorithm.study.test25;
+
+/**
+ * @author :  chen weijie
+ * @Date: 2019-09-02 23:06
+ * 参考 Solution3
+ */
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
